7203ba394525ca0e4a7587cae11e01d890a7cd01
Maruf-dev <https://github.com/Maruf-dev>
2023-10-22 11:48:46 +0300

    ready 01

---
+++ blocks/footer.php
@@ -0,0 +1,137 @@
+<footer class="footer widget-footer clearfix">
+    <div class="first-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 widget-area">
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 widget-area">
+                    <div class="featured-icon-box icon-align-before-content">
+                        <div class="featured-icon">
+                            <div class="ttm-icon ttm-icon_element-onlytxt ttm-icon_element-color-darkgrey ttm-icon_element-size-lg">
+                                <i class="flaticon flaticon-message"></i>
+                            </div>
+                        </div>
+                        <div class="featured-content">
+                            <div class="featured-title">
+                                <h3>Yangilik va E'lonlardan</h3>
+                            </div>
+                            <div class="featured-desc">
+                                <p>xabardor bo'lish uchun obuna bo'ling.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 widget-area">
+                    <div class="widget newsletter_widget clearfix res-991-padding_top30">
+                        <form id="subscribe-form" class="newsletter-form" method="post" action="#" data-mailchimp="true">
+                            <div class="mailchimp-inputbox clearfix" id="subscribe-content">
+                                <p><input type="email" name="email" placeholder="Elektron pochtangizni kiriting" required=""></p>
+                                <button class="submit ttm-btn ttm-btn-size-md ttm-btn-shape-square ttm-btn-style-fill ttm-btn-color-darkgrey" type="submit">Yuborish<i class="fa fa-long-arrow-right"></i></button>
+                            </div>
+                            <div id="subscribe-msg"></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="second-footer ttm-bgimage-yes bg-footer ttm-bg ttm-bgcolor-darkgrey">
+        <div class="ttm-row-wrapper-bg-layer ttm-bg-layer"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 widget-area">
+                    <div class="widget-latest-tweets mt_140 res-991-margin_top0 clearfix">
+                        <div class="widgte-text">
+                            <div class="widgte-title">
+                                <h4>Opening Hours</h4>
+                            </div>
+                            <ul class="ttm-timelist-block">
+                                <li>Mon-Friday<span class="service-time">09:00 - 20:00</span></li>
+                                <li>Saturday<span class="service-time">09:00 - 12:00</span></li>
+                                <li>Sunday <span class="sundaylist">Close</span></li>
+                            </ul>
+                            <div class="widgte-title">
+                                <h4>Biz haqimizda</h4>
+                            </div>
+                            <div class="">
+                                <p>BuildTab is established in 1984, the world’s most trusted infrastructure and
+                                    consulting firm in world wide.</p>
+                            </div>
+                            <div class="widget_social padding_top10 clearfix">
+                                <div class="social-icons">
+                                    <ul class="social-icons list-inline">
+                                        <li><a class="tooltip-top" href="#" rel="noopener" aria-label="facebook" data-tooltip="Facebook">
+                                                <i class="fa fa-facebook"></i></a></li>
+                                        <li><a class="tooltip-top" href="#" rel="noopener" aria-label="instagram" data-tooltip="instagram">
+                                                <i class="fa fa-instagram"></i></a></li>
+                                        <li><a class="tooltip-top" href="#" rel="noopener" aria-label="Telegram" data-tooltip="Telegram">
+                                                <i class="fa fa-paper-plane"></i></a></li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 widget-area">
+                    <div class="widget widget_nav_menu clearfix">
+                        <h3 class="widget-title">Mahsulotlarizmiz</h3>
+                        <ul id="menu-footer-quick-links">
+                            <li><a href="#"> <i class="fa fa-arrow-right"></i>About Company</a></li>
+                            <li><a href="#"><i class="fa fa-arrow-right"></i>Free Consultation</a></li>
+                            <li><a href="#"><i class="fa fa-arrow-right"></i>Our Services</a></li>
+                            <li><a href="#"><i class="fa fa-arrow-right"></i>Chemistry</a></li>
+                            <li><a href="#"><i class="fa fa-arrow-right"></i>Forensic science</a></li>
+                            <li><a href="#"><i class="fa fa-arrow-right"></i>Customer Insights</a></li>
+                            <li><a href="#"><i class="fa fa-arrow-right"></i>Meet Our Team</a></li>
+                            <li><a href="#"><i class="fa fa-arrow-right"></i>Scientific</a></li>
+                            <li><a href="#"><i class="fa fa-arrow-right"></i>Gemological</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 widget-area">
+                    <div class="widget widget-recent-post clearfix">
+                        <h3 class="widget-title">Recent Posts</h3>
+                        <ul class="widget-post ttm-recent-post-list">
+                            <li>
+                                <a href="blog-right-sidebar.html"><img class="img-fluid" src="images/blog/b_thumbb-01.jpg" alt="post-img"></a>
+                                <div class="post-detail">
+                                    <span class="post-date"><i class="fa fa-calendar"></i>May 21, 2021</span>
+                                    <a href="blog-right-sidebar.html">Improve Workflow With Agile <br>
+                                        Construction</a>
+                                </div>
+                            </li>
+                            <li>
+                                <a href="blog-right-sidebar.html"><img class="img-fluid" src="images/blog/b_thumbb-02.jpg" alt="post-img"></a>
+                                <div class="post-detail">
+                                    <span class="post-date"><i class="fa fa-calendar"></i>Apri 28, 2021</span>
+                                    <a href="blog-right-sidebar.html">What Are The Benefits Of LEED <br>
+                                        Certification?</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bottom-footer-text ttm-bg copyright">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="text-left">
+                            <span class="cpy-text"> Copyright © <?= date('Y') ?> <a href="#" class=" font-weight-500">
+                                    Buildtab </a> Template by &nbsp;<a href="https://themetechmount.com/">Themetechmount</a></span>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <ul class="footer-nav-menu text-end">
+                            <li><a href="about-us.html">Biz haqimizda</a></li>
+                            <li><a href="contact-us.html">Mahsulotlar</a></li>
+                            <li><a href="contact-us.html">Narxlar jadvali</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ blocks/header.php
@@ -0,0 +1,139 @@
+<header id="masthead" class="header ttm-header-style-01 fixed-header">
+    <!-- topbar -->
+    <div class="top_bar ttm-bgcolor-darkgrey clearfix">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="top_bar_contact_item">
+                                <div class="top_bar_icon"><i class="flaticon flaticon-placeholder"></i></div>
+                                <div class="top_bar_content">81 Bank Road Saginaw, MI 48601</div>
+                            </div>
+                            <div class="top_bar_contact_item">
+                                <div class="top_bar_icon"><i class="flaticon flaticon-envelope"></i></div>
+                                <div class="top_bar_content"> <a href="mailto:info@example.com">info@example.com</a></div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="contact-info">
+                                <div class="top_bar_contact_item top_bar_social ml-auto p-0">
+                                    <ul class="social-icons d-flex">
+                                        <li>
+                                            <a class="ttm-social-facebook" href="https://www.facebook.com/themetechMount18" rel="noopener" aria-label="facebook"><i class="fa fa-facebook-f"></i></a>
+                                        </li>
+
+                                        <li>
+                                            <a class="ttm-social-linkedin" href="#" rel="noopener" aria-label="google"><i class="fa fa-paper-plane"></i></a>
+                                        </li>
+                                        <li>
+                                            <a class="ttm-social-pinteres" href="https://in.pinterest.com/themetechmount/_created/" rel="noopener" aria-label="google"><i class='fa fa-instagram'></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="top_bar_contact_item ttm-highlight-right">
+                                    <div class="top_bar_icon"><i class="flaticon-phone-call"></i></div>
+                                    <div class="top_bar_content"> Murojaat uchun:
+                                        <strong><a href="tel: +998973288888">+998 (97) 328-88-88</a></strong>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- topbar end -->
+    <!-- site-header-menu -->
+    <div id="site-header-menu" class="site-header-menu ttm-bgcolor-white">
+        <div class="site-header-menu-inner ttm-stickable-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 ">
+                        <!--site-navigation -->
+                        <div class="site-navigation d-flex flex-row  justify-content-between align-items-center">
+                            <!-- site-branding -->
+                            <div class="site-branding ">
+                                <a class="home-link" href="/" title="Oqqurg'onlik" rel="home">
+                                    <img id="logo-img" class="img-fluid auto_size" src="images/logo/logo.jpg" alt="logo-img">
+                                </a>
+                            </div><!-- site-branding end -->
+                            <div class=" d-flex flex-row ">
+                                <div class="btn-show-menu-mobile menubar menubar--squeeze">
+                                    <span class="menubar-box">
+                                        <span class="menubar-inner"></span>
+                                    </span>
+                                </div>
+                                <!-- menu -->
+                                <nav class="main-menu menu-mobile" id="menu">
+                                    <ul class="menu">
+                                        <li class="mega-menu-item active">
+                                            <a href="/">Bosh sahifa</a>
+                                        </li>
+                                        <li class="mega-menu-item">
+                                            <a href="#" class="mega-menu-link">Biz haqimizda</a>
+                                            <ul class="mega-submenu">
+                                                <li><a href="about-us.html">Tashkil topish tarixi</a></li>
+                                                <li><a href="about-us-2.html">Rahbariyat</a></li>
+                                                <li><a href="services-1.html">Rivojlarnish Dinamikasi</a></li>
+                                                <li><a href="services-2.html">Tenderlar</a></li>
+                                                <li><a href="our-team.html">Yangiliklar</a></li>
+                                                <li><a href="our-team.html">Ish o'rinlari</a></li>
+
+                                            </ul>
+                                        </li>
+                                        <li class="mega-menu-item">
+                                            <a href="#" class="mega-menu-link">Mahsulotlar</a>
+                                            <ul class="mega-submenu">
+                                                <li><a href="general-builder.html">General Builder</a></li>
+                                                <li><a href="house-extensions.html">House Extensions</a></li>
+                                                <li><a href="electrical-services.html">Electrical Services</a>
+                                                </li>
+                                                <li><a href="civil-structure.html">Civil Structure</a></li>
+                                                <li><a href="multistory-build.html">Multistory Build</a></li>
+                                                <li><a href="disaster-response.html">Disaster Response</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="mega-menu-item">
+                                            <a href="#">Narxlar jadvali</a>
+
+                                        </li>
+
+                                        <li class="mega-menu-item">
+                                            <a href="#"><img src="./images/lang/ru.png" width="30px" height="auto" alt="ru"></a>
+                                        </li>
+                                    </ul>
+                                </nav><!-- menu end -->
+                                <div class="header_extra d-flex flex-row align-items-center justify-content-end">
+                                    <div class="header_search">
+                                        <a href="#" class="btn-default search_btn" rel="noopener" aria-label="searchbox"><i class="ti ti-search"></i></a>
+                                        <div class="header_search_content">
+                                            <form id="searchbox" method="get" action="#">
+                                                <input class="search_query" type="text" id="search_query_top" name="s" placeholder=".." value="">
+                                                <button type="submit" class="btn close-search" aria-label="searchbox-align"><i class="fa fa-search"></i></button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                    <div class="padding_left15">
+                                        <div class="header_btn">
+                                            <a class="ttm-btn ttm-btn-size-md ttm-text-darkgrey btn-inline ttm-icon-btn-right" href="contact-us.html">Aloqa uchun
+                                                <i class="fa fa-long-arrow-right"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- site-navigation end-->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- site-header-menu end-->
+</header>
+
+<?php
+
+
+
+?>
